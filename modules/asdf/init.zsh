36a840007b6438fc40c420957918494db79dbda2
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2020-02-25 14:57:06 +0000

    Fix asdf

--- modules/asdf/init.zsh
+++ modules/asdf/init.zsh
@@ -8,6 +8,6 @@
 # ASDF uses its master branch for dev and puts out usable releases wiht release tags
 # As we can't know what the latest tag is in advance, this uses git to go into the
 # external repo and cehck out whatever the latest tag we know of might be.
-git -C ${0:h}/external checkout $(git -C ${0:h}/external describe --abbrev=0 --tags)
+git -C $HOME/.dotfiles/zsh/prezto/modules/asdf/external checkout $(git -C $HOME/.dotfiles/zsh/prezto/modules/asdf/external describe --abbrev=0 --tags)
 
-sh '${0:h}/external/asdf.sh'
+sh '$HOME/.dotfiles/zsh/prezto/modules/asdf/external/asdf.sh'
