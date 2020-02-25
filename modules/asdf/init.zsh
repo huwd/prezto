0e40b3881d0e20c81a49ed11e576f90e759f3d23
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2020-02-25 14:57:06 +0000

    Fix asdf

---
+++ modules/asdf/init.zsh
@@ -0,0 +1,50 @@
+#
+# Integrates asdf into Prezto.
+#
+# Authors:
+#   Huw Diprose <mail+prezto@huwdiprose.co.uk>
+#
+
+# ASDF uses its master branch for dev and puts out usable releases wiht release tags
+# As we can't know what the latest tag is in advance, this uses git to go into the
+# external repo and cehck out whatever the latest tag we know of might be.
+git -C $HOME/.dotfiles/zsh/prezto/modules/asdf/external checkout $(git -C $HOME/.dotfiles/zsh/prezto/modules/asdf/external describe --abbrev=0 --tags)
+
+# For Korn shells (ksh, mksh, etc.), capture $_ (the final parameter passed to
+# the last command) straightaway, as it will contain the path to this script.
+# For Bash, ${BASH_SOURCE[0]} will be used to obtain this script's path.
+# For Zsh and others, $0 (the path to the shell or script) will be used.
+_under="$_"
+if [ "${BASH_SOURCE[0]}" != "" ]; then
+  current_script_path="${BASH_SOURCE[0]}"
+elif [[ "$_under" == *".sh" ]]; then
+  current_script_path="$_under"
+else
+  current_script_path="$0"
+fi
+
+export ASDF_DIR
+ASDF_DIR="$(dirname "$current_script_path")"
+# shellcheck disable=SC2016
+[ -d "$ASDF_DIR" ] || echo '$ASDF_DIR is not a directory'
+
+# Add asdf to PATH
+#
+# if in $PATH, remove, regardless of if it is in the right place (at the front) or not.
+# replace all occurrences - ${parameter//pattern/string}
+ASDF_BIN="${ASDF_DIR}/bin"
+ASDF_USER_SHIMS="${ASDF_DATA_DIR:-$HOME/.asdf}/shims"
+[[ ":$PATH:" == *":${ASDF_BIN}:"* ]] && PATH="${PATH//$ASDF_BIN:/}"
+[[ ":$PATH:" == *":${ASDF_USER_SHIMS}:"* ]] && PATH="${PATH//$ASDF_USER_SHIMS:/}"
+# add to front of $PATH
+PATH="${ASDF_BIN}:$PATH"
+PATH="${ASDF_USER_SHIMS}:$PATH"
+
+# shellcheck source=lib/asdf.sh
+# Load the asdf wrapper function
+. "${ASDF_DIR}/lib/asdf.sh"
+
+if [ -n "$ZSH_VERSION" ]; then
+  autoload -U bashcompinit
+  bashcompinit
+fi
