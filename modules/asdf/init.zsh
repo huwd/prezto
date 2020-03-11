4c5742cbbb07f81d16dca30c454340c90f68dd59
huw <huw@grey-area.local>
2020-03-11 14:06:21 +0000

    Fix checkout actually firing during asdf install message

--- modules/asdf/init.zsh
+++ modules/asdf/init.zsh
@@ -3,12 +3,10 @@ git clone https://github.com/asdf-vm/asdf.git ~/.dotfiles/asdf
 
 ln -nsf ~/.dotfiles/asdf ~/.asdf
 
-echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
-
 echo "=================================================="
 echo ""
 echo "Now cd into ~/.asdf"
 echo "And checkout the lastest tag with:"
-echo "git checkout \"$(git describe --abbrev=0 --tags)\""
+echo 'git checkout \"$(git describe --abbrev=0 --tags)\"'
 echo ""
 echo "=================================================="
